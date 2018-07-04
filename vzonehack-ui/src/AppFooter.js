b0c453ddab021706d66c1a036da99ceed0994445
thangay <manoj.hicet@gmail.com>
2018-07-04 09:42:14 +0000

    Register User Changes

--- vzonehack-ui/src/AppFooter.js
+++ vzonehack-ui/src/AppFooter.js
@@ -5,8 +5,8 @@ export class AppFooter extends Component {
     render() {
         return  (
             <div className="layout-footer">
-                <img src="assets/layout/images/logo.svg" alt="" width="80"/>
-                <span className="footer-text" style={{'marginRight': '5px'}}>One Hack</span>
+                <span className="footer-text" style={{'marginRight': '5px'}}>Proprietary and Confidential. Not for discloser outside of Verizon.</span>
+                <span className="footer-text" style={{'margin-left': '50%'}}>Copyright 2018</span>
             </div>
         );
     }

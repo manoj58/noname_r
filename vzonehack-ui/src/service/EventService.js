1991f40fe1f0b60164283d7dca439282fb529815
manoj58 <manoj.hicet@gmail.com>
2018-07-04 10:45:44 +0000

    Event Service

--- vzonehack-ui/src/service/EventService.js
+++ vzonehack-ui/src/service/EventService.js
@@ -26,6 +26,104 @@ export class EventService {
                  likesCount
                }
              } `;
+    ADD_EVENT = gql`
+             mutation CreateEventQuery($eventName: String!, $eventType: String!, $eventStartDate: Date!, $eventEndDate: Date!, $nominationStartDate: Date!, $nominationEndDate: Date!, $minTeamSize: Int!, $maxTeamSize: Int!, $eventPortfolio: String!, $eventLocation: String!, $eventPOCMail: String!) {
+                 addEvent(eventName: $eventName, eventType: $eventType, eventStartDate: $eventStartDate, eventEndDate: $eventEndDate, nominationStartDate: $nominationStartDate, nominationEndDate: $nominationEndDate, minTeamSize: $minTeamSize, maxTeamSize: $maxTeamSize, eventPortfolio: $eventPortfolio, eventLocation: $eventLocation, eventPOCMail: $eventPOCMail, viewCount: 0, likesCount: 0) {
+                   eventName
+                   eventType
+                   eventStartDate
+                   eventEndDate
+                   nominationStartDate
+                   nominationEndDate
+                   minTeamSize
+                   maxTeamSize
+                   eventPortfolio
+                   eventLocation
+                   eventPOCMail
+                   viewCount
+                   likesCount
+                 }
+               } `;
+
+    GET_UPCOMING_EVENTS = gql`
+               query getUpcomingEvents {
+                   getUpcomingEvents {
+                     eventId
+                     eventName
+                     eventType
+                     eventStartDate
+                     eventEndDate
+                     nominationStartDate
+                     nominationEndDate
+                     minTeamSize
+                     maxTeamSize
+                     eventPortfolio
+                     eventLocation
+                     eventPOCMail
+                     viewCount
+                     likesCount
+                   }
+                 } `;
+
+    GET_ONGOING_EVENTS = gql`
+                 query getOngoingEvents {
+                     getOngoingEvents {
+                       eventId
+                       eventName
+                       eventType
+                       eventStartDate
+                       eventEndDate
+                       nominationStartDate
+                       nominationEndDate
+                       minTeamSize
+                       maxTeamSize
+                       eventPortfolio
+                       eventLocation
+                       eventPOCMail
+                       viewCount
+                       likesCount
+                     }
+                   } `;
+
+    GET_EVENTS_PORTFOLIO = gql`
+                   query getEventsByPortfolio($portfolio: String!) {
+                       getEventsByPortfolio(portfolio: $portfolio) {
+                           eventId
+                           eventName
+                           eventType
+                           eventStartDate
+                           eventEndDate
+                           nominationStartDate
+                           nominationEndDate
+                           minTeamSize
+                           maxTeamSize
+                           eventPortfolio
+                           eventLocation
+                           eventPOCMail
+                           viewCount
+                           likesCount
+                       }
+                   } `;
+
+    GET_EVENT_LOCATION = gql`
+        query getEventsByLocation($location: String!) {
+            getEventsByLocation(location: $location) {
+                eventId
+                eventName
+                eventType
+                eventStartDate
+                eventEndDate
+                nominationStartDate
+                nominationEndDate
+                minTeamSize
+                maxTeamSize
+                eventPortfolio
+                eventLocation
+                eventPOCMail
+                viewCount
+                likesCount
+            }
+        } `;
 
     getEvents() {
         return axios.get('assets/demo/data/scheduleevents.json')
@@ -33,9 +131,57 @@ export class EventService {
     }
 
     getAllEvents() {
-        console.log("Calling getAllEvents");
         return this.client.query({
             query: this.GET_EVENTS
+        }).then(results => results.data);
+    }
+
+    getUpcomingEvents() {
+        return this.client.query({
+            query: this.GET_UPCOMING_EVENTS
+        }).then(results => results.data);
+    }
+
+    getOngoingEvents() {
+        return this.client.query({
+            query: this.GET_ONGOING_EVENTS
+        }).then(results => results.data);
+    }
+
+    getEventsbyPortfolio(portfolio) {
+        return this.client.query({
+            query: this.GET_EVENTS_PORTFOLIO,
+            variables: {
+                "portfolio": portfolio
+            }
+        }).then(results => results.data);
+    }
+
+    getEventsbyLocation(location) {
+        return this.client.query({
+            query: this.GET_EVENT_LOCATION,
+            variables: {
+                "location": location
+            }
+        }).then(results => results.data);
+    }
+
+    addEvent(eventName, eventType, eventStartDate, eventEndDate, nominationStartDate, nominationEndDate, minTeamSize, maxTeamSize, eventPortfolio, eventLocation, eventPOCMail) {
+        return this.client.mutate({
+            mutation: this.ADD_EVENT,
+            variables: {
+                "eventName": eventName,
+                "eventType": eventType,
+                "eventStartDate": eventStartDate,
+                "eventEndDate": eventEndDate,
+                "nominationStartDate": nominationStartDate,
+                "nominationEndDate": nominationEndDate,
+                "minTeamSize": minTeamSize,
+                "maxTeamSize": maxTeamSize,
+                "eventPortfolio": eventPortfolio,
+                "eventLocation": eventLocation,
+                "eventPOCMail": eventPOCMail
+            }
         }).then(results => results.data);
     }
 }
